6282802e938850e8abddbbaae618879d86174f54
Jingxian Wang <34795160+wjxway@users.noreply.github.com>
2020-05-29 02:46:22 +0800

    Add files via upload

---
+++ PhysicalColor/PacletInfo.m
@@ -0,0 +1,16 @@
+(* ::Package:: *)
+
+(* Created with the Wolfram Language : www.wolfram.com *)
+Paclet[
+    Name -> "PhysicalColor",
+    Version -> "0.1.0",
+    WolframVersion -> "11.3+",
+    MathematicaVersion -> "11.3+",
+    Description -> "A Mathematica package that provides better visible spectrum and blackbody radiation color.",
+	Creator -> "Jingxian Wang",
+	License -> "WTFPL",
+    Extensions -> 
+    {
+		{"Kernel", Root -> ".", Context -> "PhysicalColor`"}
+	}
+]
